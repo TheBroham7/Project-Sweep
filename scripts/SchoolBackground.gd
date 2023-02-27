35ef27ea810f37f3002bc85b74355a5bd565e234
kori375 <kori.schwarz@utexas.edu>
2023-02-27 01:17:37 -0600

    rat disappearing on swing/player health/ health counter
    added more code for the rat and player collision. deleted 2 scripts that aren't relevant (mobfollow and mobpath). added health counter

--- scripts/SchoolBackground.gd
+++ scripts/SchoolBackground.gd
@@ -1,5 +1,4 @@
 extends ParallaxBackground
-signal stop
 
 export (float) var scrolling_speed = 150.0
 
@@ -10,9 +9,4 @@ func _ready():
 
 func _process(delta):
 	scroll_offset.y += scrolling_speed * delta
-
-func _on_Player_body_entered(_body):
-	scrolling_speed = 0
-	emit_signal("stop")
-
 
