692d3b80f277ef6b426d3f14705857f459fb2a27
kori375 <kori.schwarz@utexas.edu>
2023-03-06 14:14:39 -0600

    Fixed some aspects of boss
    still can't be killed, but throws books which damage player

--- scenes/book_projectile.gd
+++ scenes/book_projectile.gd
@@ -1,6 +1,16 @@
 extends RigidBody2D
 
 var velocity = Vector2(0.0, rand_range(50.0, 100.0))
+var pushback = false
 
 func _process(delta):
-	global_position += velocity * delta
+	if pushback == false: 
+		global_position -= velocity * delta
+	elif pushback == true: 
+		$PushBackTimer.start()
+		global_position += velocity*delta
+	pass
+
+
+func _on_PushBackTimer_timeout():
+	pushback = false
